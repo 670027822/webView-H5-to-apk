341ba18d7ae8928523b046f3aa844a362bdcbeb3
bewilder-cell <1761877578@qq.com>
2025-04-15 18:18:01 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.test;
-
 import android.annotation.SuppressLint;
 import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
@@ -11,6 +9,7 @@ import android.content.IntentFilter;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -24,8 +23,6 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
-import android.os.Handler;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -37,6 +34,8 @@ import com.example.test.LocalService;
 import com.example.test.RemoteService;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,31 +45,27 @@ public class MainActivity extends AppCompatActivity {
     ValueCallback<Uri[]> mFilePathCallbackArray;
     private static final int JOB_ID = 100;
 
-    // 新增：定时切换 URL 的相关字段
-    private final String[] urlList = {
-        "https://baidu.com",
-        "https://www.wenshushu.cn/"
-    };
-    private int currentUrlIndex = 0;
-    private Handler urlSwitchHandler = new Handler();
-
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //隐藏ActionBar
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
-
+        //WebView加载页面
         webView = findViewById(R.id.web_view);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
-
+        // code from https://blog.csdn.net/qq_21138819/article/details/56676007 by 欢子-3824
         webView.setWebChromeClient(new WebChromeClient() {
+            // Andorid 4.1----4.4
             public void openFileChooser(ValueCallback<Uri> uploadFile, String acceptType, String capture) {
+
                 mFilePathCallback = uploadFile;
                 handle(uploadFile);
             }
 
+            // for 5.0+
             public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
                 if (mFilePathCallbackArray != null) {
                     mFilePathCallbackArray.onReceiveValue(null);
@@ -82,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
             private void handle(ValueCallback<Uri> uploadFile) {
                 Intent intent = new Intent(Intent.ACTION_PICK);
+                // 设置允许上传的文件类型
                 intent.setType("*/*");
                 startActivityForResult(intent, PICK_REQUEST);
             }
@@ -93,30 +89,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        webView.setDownloadListener((url, userAgent, contentDisposition, mimetype, contentLength) -> {
-            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-            String cookies = CookieManager.getInstance().getCookie(url);
-            request.addRequestHeader("cookie", cookies);
-            request.addRequestHeader("User-Agent", userAgent);
-            request.setDescription("下载中...");
-            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
-            request.allowScanningByMediaScanner();
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype));
+        // wevView监听 H5 页面的下载事件
+        // code from https://github.com/madhan98/Android-webview-upload-download/blob/master/app/src/main/java/com/my/newproject/MainActivity.java by Madhan
+        webView.setDownloadListener(new DownloadListener() {
 
-            DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-            manager.enqueue(request);
-            showMessage("下载中...");
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
 
-            BroadcastReceiver onComplete = new BroadcastReceiver() {
-                public void onReceive(Context ctxt, Intent intent) {
-                    showMessage("下载完成");
-                    unregisterReceiver(this);
-                }
-            };
-            registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+
+                String cookies = CookieManager.getInstance().getCookie(url);
+
+                request.addRequestHeader("cookie", cookies);
+
+                request.addRequestHeader("User-Agent", userAgent);
+
+                request.setDescription("下载中...");
+
+                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+
+                request.allowScanningByMediaScanner(); request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype));
+
+                DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+
+                manager.enqueue(request);
+
+                showMessage("下载中...");
+
+                //Notif if success
+
+                BroadcastReceiver onComplete = new BroadcastReceiver() {
+
+                    public void onReceive(Context ctxt, Intent intent) {
+
+                        showMessage("下载完成");
+
+                        unregisterReceiver(this);
+
+                    }};
+
+                registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+
+            }
+
         });
 
+        //该方法解决的问题是打开浏览器不调用系统浏览器，直接用 webView 打开
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -124,48 +141,38 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        // 获取当前时间戳
+long timestamp = System.currentTimeMillis();
 
-        // 加载初始页面
-        long timestamp = System.currentTimeMillis();
-        webView.loadUrl(urlList[0] + "?t=" + timestamp);
+// 这里填你需要打包的 H5 页面链接，并附加时间戳参数
+String url = "http://10.114.136.173:8082/pages/views/pickTemplate/pickStateTWMaterial?t=" + timestamp;
 
+        // 这里填你需要打包的 H5 页面链接
+        webView.loadUrl(url);
+
+        //显示一些小图片（头像）
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         }
+        // 允许使用 localStorage sessionStorage
         webView.getSettings().setDomStorageEnabled(true);
+        // 是否支持 html 的 meta 标签
         webView.getSettings().setUseWideViewPort(true);
         webView.getSettings().setAllowFileAccess(true);
         webView.getSettings().getAllowUniversalAccessFromFileURLs();
         webView.getSettings().getAllowFileAccessFromFileURLs();
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
 
         startForegroundService();
-        startUrlSwitchTimer(); // 启动定时器
+
+        webView.loadUrl("javascript:(function() { " +
+            "var audio = document.getElementById('alarmSound');" +
+            "audio.load();" +
+            "})()");
     }
 
-    private void startUrlSwitchTimer() {
-        urlSwitchHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                currentUrlIndex = (currentUrlIndex + 1) % urlList.length;
-                long timestamp = System.currentTimeMillis();
-                String newUrl = urlList[currentUrlIndex] + "?t=" + timestamp;
-                webView.loadUrl(newUrl);
-                urlSwitchHandler.postDelayed(this, 30000); // 30 秒
-            }
-        }, 30000); // 首次延迟
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        urlSwitchHandler.removeCallbacksAndMessages(null); // 清除定时器
-        if (webView != null) {
-            webView.destroy();
-            webView = null;
-        }
-    }
-
+    //设置回退页面
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
             webView.goBack();
@@ -179,6 +186,14 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        webView.destroy();
+        webView = null;
+    }
+
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
@@ -187,17 +202,28 @@ public class MainActivity extends AppCompatActivity {
                 Uri uri = data.getData();
                 handleCallback(uri);
             } else {
+                // 取消了照片选取的时候调用
                 handleCallback(null);
             }
         } else {
+            // 取消了照片选取的时候调用
             handleCallback(null);
         }
     }
 
+    /**
+     * 处理WebView的回调
+     *
+     * @param uri
+     */
     private void handleCallback(Uri uri) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             if (mFilePathCallbackArray != null) {
-                mFilePathCallbackArray.onReceiveValue(uri != null ? new Uri[]{uri} : null);
+                if (uri != null) {
+                    mFilePathCallbackArray.onReceiveValue(new Uri[]{uri});
+                } else {
+                    mFilePathCallbackArray.onReceiveValue(null);
+                }
                 mFilePathCallbackArray = null;
             }
         } else {
@@ -205,6 +231,7 @@ public class MainActivity extends AppCompatActivity {
                 if (uri != null) {
                     String url = getFilePathFromContentUri(uri, getContentResolver());
                     Uri u = Uri.fromFile(new File(url));
+
                     mFilePathCallback.onReceiveValue(u);
                 } else {
                     mFilePathCallback.onReceiveValue(null);
@@ -217,8 +244,13 @@ public class MainActivity extends AppCompatActivity {
     public static String getFilePathFromContentUri(Uri selectedVideoUri, ContentResolver contentResolver) {
         String filePath;
         String[] filePathColumn = {MediaStore.MediaColumns.DATA};
+
         Cursor cursor = contentResolver.query(selectedVideoUri, filePathColumn, null, null, null);
+//      也可用下面的方法拿到cursor
+//      Cursor cursor = this.context.managedQuery(selectedVideoUri, filePathColumn, null, null, null);
+
         cursor.moveToFirst();
+
         int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
         filePath = cursor.getString(columnIndex);
         cursor.close();
@@ -226,22 +258,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startForegroundService() {
+        // 启动前台服务
         Intent serviceIntent = new Intent(this, ForegroundService.class);
         startService(serviceIntent);
+        
+        // 启动双进程保活服务
         startService(new Intent(this, LocalService.class));
         startService(new Intent(this, RemoteService.class));
+        
+        // 设置并启动 JobScheduler
         scheduleJob();
     }
-
+    
     private void scheduleJob() {
         ComponentName serviceComponent = new ComponentName(this, JobSchedulerService.class);
         JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceComponent);
+        
+        // 设置任务在网络可用时执行
         builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
+        
+        // 设置任务在设备充电时执行
         builder.setRequiresCharging(true);
+        
+        // 设置任务的最小延迟时间（3分钟）
         builder.setMinimumLatency(3 * 60 * 1000);
+        
+        // 设置任务的最大延迟时间（10分钟）
         builder.setOverrideDeadline(10 * 60 * 1000);
+        
+        // 设置在设备重启后是否继续执行
         builder.setPersisted(true);
-
+        
         JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
         if (jobScheduler != null) {
             int resultCode = jobScheduler.schedule(builder.build());
@@ -250,4 +297,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
 }
