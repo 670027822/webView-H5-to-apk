41d9966dffbb1294349c28ec520636ddfa33c247
bewilder-cell <1761877578@qq.com>
2025-05-16 10:20:13 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -131,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setAllowFileAccess(true);
         webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
 
-        String url = "http://10.114.136.173:8082/#/";
+        String url = "http://10.114.136.173:8082/#/pages/views/pickTemplate/pickStateTWSB";
         webView.loadUrl(url);
     }
 
