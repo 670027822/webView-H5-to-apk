a22222d2f5bae51396a546f1a105c3a4bc8408dc
bewilder-cell <1761877578@qq.com>
2025-05-16 10:04:12 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -131,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setAllowFileAccess(true);
         webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
 
-        String url = "https://www.baidu.com";
+        String url = "http://10.114.136.173:8282/#/";
         webView.loadUrl(url);
     }
 
